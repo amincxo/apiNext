273a967392a211ff28a0a18a0eda260b554d8855
unknown <amincxo@gmail.com>
2025-04-28 17:30:17 +0330

    post api oin next complate

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -1,3 +1,19 @@
+import { todos } from "../../../data/todos"
+
 export default function (req , res) {
-    res.status(200).json({name: '/api/todos route'})
+    if(req.method === 'GET'){
+        res.status(200).json(todos)
+    } else if (req.method === 'POST') {
+        const {todo} = req.body;
+        //store data in db
+        const newTodo = {
+            id : todos.length +1 ,
+            title: todo,
+        };
+        res.status(201).json({
+            message: 'create ucsefuly',
+            data : newTodo
+        })
+    }
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react"
+
+
 export default function Home() {
-  return (
+
+    const [todos , setTodos] = useState([])
+    const [todo , setTodo] = useState([])
+
+    useEffect(()=> {
+        async function fetchData() {
+            const res = await fetch('/api/todos')
+            const data = await res.json()
+            setTodos(data);
+        }
+        fetchData();
+    },[])
+
+    const clickHandler = async () => {
+        const res = fetch('/api/todos', {
+            method: 'POST',
+            body: JSON.stringify({todo}),
+            headers: {'Content-Type': 'application/json'}
+        });
+        const data = await (await res).json()
+        console.log(data)
+    }
+
+    return (
     <>
      <h1>salam</h1>
+    <ul>
+        {todos.map((todo)=> (
+            <li key={todo.id} >{todo.title}</li>
+        ))}
+    </ul>
+    <div>
+        <input value={todo} onChange={e => setTodo(e.target.value)} />
+        <button onClick={clickHandler} >create Todo</button>
+    </div>
     </>
   )
 }
